94198356160b4cae9e335ee9d8bc626a88364ba0
TallulahDrn <tallulah.dreno44@gmail.com>
2024-12-30 12:39:57 +0100

    iuytfd

--- database/tables.sql
+++ database/tables.sql
@@ -3,82 +3,51 @@
 -- Table : Personne
 CREATE TABLE Personne (
     id SERIAL PRIMARY KEY,
-    nom VARCHAR(1000),
-    prenom VARCHAR(100),
-    email VARCHAR(100),
-    mot_de_passe VARCHAR(100),
-    medecin BOOLEAN,
-    telephone VARCHAR(20)
+    nom VARCHAR(50) NOT NULL,
+    prenom VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    mot_de_passe VARCHAR(50) NOT NULL,
+    medecin BOOLEAN NOT NULL,
+    telephone VARCHAR(20) NOT NULL
 );
 
 -- Table : rendez vous
-CREATE TABLE rdv (
+CREATE TABLE Rdv (
     id SERIAL PRIMARY KEY,
-    heure TIME,
-    duree TIME,
-    date DATE
+    heure TIME NOT NULL,
+    duree TIME NOT NULL,
+    date DATE NOT NULL,
+    id_personne INT FOREIGN KEY REFERENCES Personne(id),
+    id_medecin INT NOT NULL FOREIGN KEY REFERENCES Medecin(id),
+    id_etablissement INT NOT NULL FOREIGN KEY REFERENCES Etablissement(id)
 );
 
 -- Table : Etablissement
 CREATE TABLE Etablissement (
     id SERIAL PRIMARY KEY,
-    adresse CHAR(50)
+    adresse CHAR(50) NOT NULL
 );
 
 
 -- Table : Medecin
 CREATE TABLE Medecin (
     id SERIAL PRIMARY KEY,
-    code_postal INT
+    code_postal INT NOT NULL,
+    id_personne INT NOT NULL FOREIGN KEY REFERENCES Personne(id)
 );
 
 -- Table : Specialite
 CREATE TABLE Specialite (
-    id  SERIAL PRIMARY KEY,
-    nom_specialite VARCHAR(50)
+    id SERIAL PRIMARY KEY,
+    nom_specialite VARCHAR(50) NOT NULL
 );
 
--- Relation : prend (entre Personne et RDV)
-CREATE TABLE prend (
-    personne_id INT,
-    rdv_id INT,
-    PRIMARY KEY (personne_id, rdv_id),
-    FOREIGN KEY (personne_id) REFERENCES Personne(id),
-    FOREIGN KEY (rdv_id) REFERENCES RDV(id)
-);
-
--- Relation : donne (entre Medecin et RDV)
-CREATE TABLE donne (
-    medecin_id INT,
-    rdv_id INT,
-    PRIMARY KEY (medecin_id, rdv_id),
-    FOREIGN KEY (medecin_id) REFERENCES Medecin(id),
-    FOREIGN KEY (rdv_id) REFERENCES RDV(id)
-);
-
--- Relation : situe (entre RDV et Etablissement)
-CREATE TABLE situe (
-    rdv_id INT,
-    etablissement_id INT,
-    PRIMARY KEY (rdv_id, etablissement_id),
-    FOREIGN KEY (rdv_id) REFERENCES RDV(id),
-    FOREIGN KEY (etablissement_id) REFERENCES Etablissement(id)
-);
-
--- Relation : appartient (entre Medecin et Personne)
-CREATE TABLE appartient (
-    medecin_id INT,
-    personne_id INT,
-    PRIMARY KEY (medecin_id, personne_id),
-    FOREIGN KEY (medecin_id) REFERENCES Medecin(id),
-    FOREIGN KEY (personne_id) REFERENCES Personne(id)
-);
 
 -- Relation : possede (entre Medecin et Specialite)
 CREATE TABLE possede (
-    medecin_id INT,
-    specialite_id INT,
-    PRIMARY KEY (medecin_id, specialite_id),
-    FOREIGN KEY (medecin_id) REFERENCES Medecin(id),
-    FOREIGN KEY (specialite_id) REFERENCES Specialite(id)
+    id_medecin INT NOT NULL,
+    id_specialite INT NOT NULL,
+    PRIMARY KEY (id_medecin, id_specialite),
+    FOREIGN KEY (id_medecin) REFERENCES Medecin(id),
+    FOREIGN KEY (id_specialite) REFERENCES Specialite(id)
 );
