e84ae55e0f041634c3f32141be913df4b5ba060b
TallulahDrn <tallulah.dreno44@gmail.com>
2024-12-30 16:53:21 +0100

    inscription avec pdo

--- database/tables.sql
+++ database/tables.sql
@@ -6,9 +6,9 @@ CREATE TABLE Personne (
     nom VARCHAR(50) NOT NULL,
     prenom VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    mot_de_passe VARCHAR(50) NOT NULL,
+    mot_de_passe VARCHAR(150) NOT NULL,
     medecin BOOLEAN NOT NULL,
-    telephone VARCHAR(20) NOT NULL
+    telephone VARCHAR(14) NOT NULL --les 10 chiffres et les points
 );
 
 -- Table : Etablissement
